7288ddf39a6141a60be8dafe862f4601cfa967b8
seigtm <seigtm@gmail.com>
2021-09-22 08:26:25 +0300

    No need to include boost conan package to the project

--- conanfile.py
+++ conanfile.py
@@ -3,7 +3,7 @@ from conans import ConanFile
 
 class Polynomial(ConanFile):
     name = "Polynomial With Complex Coefficients"
-    version = "1.0.0"
+    version = "1.0.1"
     author = "Baranov Konstantin (seigtm@gmail.com)"
     url = "https://github.com/seigtm/Polynomial"
     description = "Test of the polynomial class whose coefficients are complex numbers"
@@ -11,7 +11,7 @@ class Polynomial(ConanFile):
     generators = "cmake"
 
     requires = \
-        "boost/[1.77.0]", "Polynomial/[~1.0.4]", "Complex/[~1.4.0]"
+        "Polynomial/[~1.0.4]", "Complex/[~1.4.0]"
 
     build_requires = \
         "gtest/[1.11]"
